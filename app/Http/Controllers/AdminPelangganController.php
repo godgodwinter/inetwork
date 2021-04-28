3ed433494bcc2d3b224212f1304e6f4a57ed49e6
godgodwinter <kakadz.pro@gmail.com>
2021-04-28 21:05:23 +0700

    update menu tambah pelanggan

--- app/Http/Controllers/AdminPelangganController.php
+++ app/Http/Controllers/AdminPelangganController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\pelanggan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 
 class AdminPelangganController extends Controller
 {
@@ -38,7 +40,52 @@ class AdminPelangganController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nik'=>'required',
+            'nama'=>'required',
+            'alamat'=>'required',
+            'hp'=>'required',
+            'paket_id'=>'required',
+            'tgl_gabung'=>'required',
+            'status_langganan'=>'required',
+            'kordinat_rumah'=>'required',
+            'letakserver_id'=>'required'
+        ],
+        [
+            'nik.required'=>'nik harus diisi',
+        ]);
+        //ambil nama PAKET
+        $ambilpaket = DB::table('paket')->where('id',$request->paket_id)->get();
+        foreach($ambilpaket as $ambil){
+            $paket_nama=$ambil->nama;
+        }
+        
+        //ambil nama letakserver
+        $ambilnamas4rver = DB::table('letakserver')->where('id',$request->letakserver_id)->get();
+        foreach($ambilnamas4rver as $ambil2){
+            $letakserver_nama=$ambil2->nama;
+            $letakserver_koordinat=$ambil2->koordinat;
+        }
+       // simpan
+       DB::table('pelanggan')->insert(
+        array(
+               'nik'     =>   $request->nik,
+               'nama'     =>   $request->nama,
+               'alamat'     =>   $request->alamat,
+               'hp'     =>   $request->hp,
+               'tgl_gabung'     =>   $request->tgl_gabung,
+               'paket_id'     =>   $request->paket_id,
+               'paket_nama'     =>   $paket_nama,
+               'kordinat_rumah'     =>   $request->kordinat_rumah,
+               'letakserver_id'     =>   $request->letakserver_id,
+               'status_langganan'     =>   $request->status_langganan,
+               'letakserver_nama'     =>   $letakserver_nama,
+               'letakserver_koordinat'     =>   $letakserver_koordinat,
+               'created_at'=>date("Y-m-d H:i:s"),
+               'updated_at'=>date("Y-m-d H:i:s")
+        )
+   );
+    return redirect(URL::to('/').'/admin/pelanggan')->with('status','Data berhasil di tambahkan!');
     }
 
     /**
--- resources/views/admin/pelanggan/index.blade.php
+++ resources/views/admin/pelanggan/index.blade.php
@@ -15,6 +15,18 @@
     input[type=number] {
       -moz-appearance: textfield;
     }
+    #mapCanvas {
+    width: 800px;
+    height: 400px;
+    float: left;
+  }
+  #infoPanel {
+    float: left;
+    margin-left: 10px;
+  }
+  #infoPanel div {
+    margin-bottom: 5px;
+  }
     </style>
 <!-- Data Table Css -->
 <link rel="stylesheet" type="text/css"
@@ -53,6 +65,7 @@
                 </ul>
             </div>
         </div>
+        
     </div>
 </div>
 @endsection
@@ -80,7 +93,8 @@
                 <a href="cetak" class="btn btn-sm  btn-primary" target="_blank">CETAK PDF</a>
             </div>
             <div class="col-xl-6 col-md-12 d-flex flex-row-reverse">
-                <a href="#kategori" class="btn btn-sm btn-secondary">KATEGORI</a>&nbsp;
+                <a href="{{url('/')}}/admin/paket" class="btn btn-sm btn-secondary">PAKET INTERNET</a>&nbsp;
+                <a href="{{url('/')}}/admin/letakserver" class="btn btn-sm btn-secondary">LETAK SERVER</a>&nbsp;
                 <a href="#add" class="btn btn-sm btn-secondary">TAMBAH PELANGGAN</a>&nbsp;
             </div>
         </div>
@@ -99,13 +113,18 @@
                         </tr>
                     </thead>
                     <tbody>
+                    @php
+                    $status_langganan='Non-Aktif';
+                    @endphp
                         @foreach ($datas as $data)
                             @php
 
-                                    $nominal=$data->nominal;
+                                    if($data->status_langganan=='Aktif'){
+                                        $status_langganan='Aktif';
+                                    }else{
+                                        $status_langganan='Non-Aktif';
+                                    }
 
-                                    // $newDate = date("d-m-Y", strtotime($data->tgl));
-                                    $tgl=$data->tgl;
 // dd($tgl);
 // {{ \Carbon\Carbon::parse($user->from_date)->format('d/m/Y')}}
 
@@ -118,7 +137,7 @@
                             <td>
                                 {{ \Carbon\Carbon::parse($data->tgl_gabung)->translatedFormat('d F Y')}}
                             </td>
-                            <td>{{$data->status_langganan}}</td>
+                            <td>{{$status_langganan}}</td>
                             <td>
                                 <?php
                                 $datapaket=$data->paket_id;
@@ -171,7 +190,8 @@
                     <h5 class="label label-success">TAMBAH PELANGGAN</h5>
                 </div>
                 <div class="col-xl-6 col-md-12 d-flex flex-row-reverse">
-                    <a href="#kategori" class="btn btn-sm btn-secondary">KATEGORI</a>&nbsp;
+                <a href="{{url('/')}}/admin/paket" class="btn btn-sm btn-secondary">PAKET INTERNET</a>&nbsp;
+                <a href="{{url('/')}}/admin/letakserver" class="btn btn-sm btn-secondary">LETAK SERVER</a>&nbsp;
                     <a href="#datatable" class="btn btn-sm btn-secondary">PELANGGAN</a>
                 </div>
             </div>
@@ -202,96 +222,202 @@
                                     @enderror
                                 </div>
                             </div>
-
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <label class="form-control-label" for="input-nominal">Nominal  (*</label> -
-                                    <b><label class="form-control-label" for="input-nominal" id="input-harga-label">Rp. 0 ,00</label></b>
-                                    <input type="number" name="nominal" id="input-nominal"
-                                        class="form-control form-control-alternative  @error('nominal') is-invalid @enderror"
-                                        placeholder="Contoh : 150000" value="{{old('nominal')}}" required>
-                                    @error('nominal')<div class="invalid-feedback"> {{$message}}</div>
-                                    @enderror
-                                </div>
-                            </div>
-                            <script>
-                                var format = function(num){
-                                var str = num.toString().replace("$", ""), parts = false, output = [], i = 1, formatted = null;
-                                if(str.indexOf(".") > 0) {
-                                    parts = str.split(".");
-                                    str = parts[0];
-                                }
-                                str = str.split("").reverse();
-                                for(var j = 0, len = str.length; j < len; j++) {
-                                    if(str[j] != ",") {
-                                        output.push(str[j]);
-                                        if(i%3 == 0 && j < (len - 1)) {
-                                            output.push(".");
-                                        }
-                                        i++;
-                                    }
-                                }
-                                formatted = output.reverse().join("");
-                                return("Rp" + formatted + ((parts) ? "." + parts[1].substr(0, 2) : ",-"));
-                            };
-                                $(document).ready(function() {
-                                $("#input-nominal").on('keyup', function() {
-                                    // alert("oops!");
-                                    $('#input-harga-label:last').text(format($(this).val()));
-                                });
-
-                            });
-                            </script>
-
-
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <label class="form-control-label" for="input-tgl">Tanggal (*</label>
-                                    <input type="date" name="tgl" id="input-tgl"
-                                        class="form-control form-control-alternative  @error('tgl') is-invalid @enderror"
-                                        placeholder="" value="{{old('tgl')}}" required>
-
-                                    @error('tgl')<div class="invalid-feedback"> {{$message}}</div>
+                                    <label class="form-control-label" for="input-alamat">Alamat (*</label>
+                                    <input type="text" name="alamat" id="input-alamat"
+                                        class="form-control form-control-alternative  @error('alamat') is-invalid @enderror"
+                                        placeholder="" value="{{old('alamat')}}" required>
+                                    @error('alamat')<div class="invalid-feedback"> {{$message}}</div>
                                     @enderror
                                 </div>
                             </div>
 
-                            <div class="col-lg-6 col-sm-6 col-xl-6 m-b-30">
-                                <label class="form-control-label" for="input-jk">Pilih Kategori  (*</label>
-                                <select name="jenispendapatan_id" id="input-jenispendapatan_id"
-                                    class="form-control form-control-info  @error('jenispendapatan_id') is-invalid @enderror"
+                            <div class="col-lg-3">
+                                <div class="form-group">
+                                    <label class="form-control-label" for="input-hp">No WA (*</label>
+                                    <input type="text" name="hp" id="input-hp"
+                                        class="form-control form-control-alternative  @error('hp') is-invalid @enderror"
+                                        placeholder="" value="{{old('hp')}}" required>
+                                    @error('hp')<div class="invalid-feedback"> {{$message}}</div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="form-group">
+                                    <label class="form-control-label" for="input-tgl_gabung">Tanggal Gabung(*</label>
+                                    <input type="date" name="tgl_gabung" id="input-tgl_gabung"
+                                        class="form-control form-control-alternative  @error('tgl_gabung') is-invalid @enderror"
+                                        placeholder="" value="{{old('tgl_gabung')}}" required>
+
+                                    @error('tgl_gabung')<div class="invalid-feedback"> {{$message}}</div>
+                                    @enderror
+                                </div>
+                            </div>
+                            
+
+                            <div class="col-lg-4 col-sm-4 col-xl-4 m-b-30">
+                                <label class="form-control-label" for="input-jk">Pilih Paket  (*</label>
+                                <select name="paket_id" id="input-paket_id"
+                                    class="form-control form-control-info  @error('paket_id') is-invalid @enderror"
                                     required>
                             <?php
-                                $data2s = DB::table('jenispendapatan')->get();
+                                $data2s = DB::table('paket')->orderBy('kecepatan', 'asc')->get();
                             ?>
                                 @foreach($data2s as $d2)
-                                        <option value="{{ $d2->id }}">{{ $d2->nama }}</option>
+                                        <option value="{{ $d2->id }}">{{ $d2->nama }} - {{$d2->kecepatan}} Mbps</option>
                                 @endforeach
-                                        </select> @error('jenispendapatan_id')<div class="invalid-feedback"> {{$message}}
+                                        </select> @error('paket_id')<div class="invalid-feedback"> {{$message}}
                                         </div>
                                 @enderror
                             </div>
 
+                            
+                            <div class="col-lg-4 col-sm-4 col-xl-4 m-b-30">
+                                <label class="form-control-label" for="input-jk">Pilih Letak Server  (*</label>
+                                <select name="letakserver_id" id="input-letakserver_id"
+                                    class="form-control form-control-info  @error('letakserver_id') is-invalid @enderror"
+                                    required>
+                            <?php
+                                $data3s = DB::table('letakserver')->orderBy('nama', 'asc')->get();
+                            ?>
+                                @foreach($data3s as $d3)
+                                        <option value="{{ $d3->id }}">{{ $d3->nama }}</option>
+                                @endforeach
+                                        </select> @error('letakserver_id')<div class="invalid-feedback"> {{$message}}
+                                        </div>
+                                @enderror
+                            </div> 
 
-                        </div>
-                    </div>
-                    <hr class="my-4" />
-                    <!-- Address -->
-                    <h6 class="heading-small text-muted mb-4">Aksi</h6>
-                    <div class="pl-lg-4">
-                        <div class="row">
-                            <div class="col-md-6">
+                            <div class="col-lg-4 col-sm-4 col-xl-4 m-b-30">
+                                <label class="form-control-label" for="input-jk">Pilih Status Langganan  (*</label>
+                                <select name="status_langganan" id="input-status_langganan"
+                                    class="form-control form-control-info  @error('status_langganan') is-invalid @enderror"
+                                    required>
+                                    
+                                        <option>Aktif</option>
+                                        <option>Non-Aktif</option>
+                        
+                                        </select> @error('status_langganan')<div class="invalid-feedback"> {{$message}}
+                                        </div>
+                                @enderror
+                            </div>
+
+                            
+                            <div class="col-lg-6">
                                 <div class="form-group">
+                                    <label class="form-control-label" for="input-koordinat">Kordinat Rumah (*</label>
+                                     <input type="text" name="kordinat_rumah" id="input-koordinat"  class="form-control form-control-alternative  @error('kordinat_rumah') is-invalid @enderror"
+                              placeholder="" value="{{old('kordinat_rumah')}}" required>
+                              @error('kordinat_rumah')<div class="invalid-feedback"> {{$message}}</div>
+                              @enderror
                                 </div>
                             </div>
+                            
                             <div class="col-md-6">
+                                <label class="form-control-label " for="input-koordinat">&nbsp; </label>
                                 <div class="form-group">
                                     <button type="Simpan" class="btn btn-success">Simpan</button>
                                 </div>
                             </div>
+                            
+                            <div id="mapCanvas"></div>
+                            <div id="infoPanel">
+                              <b>Marker status:</b>
+                              <div id="markerStatus"><i>Click and drag the marker.</i></div>
+                              <b>Current position:</b>
+                              <div id="info"></div>
+                              {{-- <b>Closest matching address:</b>
+                              <div id="address"></div> --}}
+                            </div><script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+
+                            <script>
+
+                                var geocoder = new google.maps.Geocoder();
+
+                                function geocodePosition(pos) {
+                                geocoder.geocode({
+                                    latLng: pos
+                                }, function(responses) {
+                                    if (responses && responses.length > 0) {
+                                    updateMarkerAddress(responses[0].formatted_address);
+                                    } else {
+                                    updateMarkerAddress('Cannot determine address at this location.');
+                                    }
+                                });
+                                }
+
+                                function updateMarkerStatus(str) {
+                                document.getElementById('markerStatus').innerHTML = str;
+                                }
+
+                                function updateMarkerPosition(latLng) {
+                                document.getElementById('info').innerHTML = [
+                                    latLng.lat(),
+                                    latLng.lng()
+                                ].join(', ');
+                                document.getElementById("input-koordinat").value = [
+                                    latLng.lat(),
+                                    latLng.lng()
+                                ].join(', ');
+                                }
+
+                                function updateMarkerAddress(str) {
+                                document.getElementById('address').innerHTML = str;
+                                }
+
+                                function initialize() {
+                                var latLng = new google.maps.LatLng(-8.129902243245665, 112.4867915739301);
+                                var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+                                    zoom: 15,
+                                    center: latLng,
+                                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                                });
+                                var marker = new google.maps.Marker({
+                                    position: latLng,
+                                    title: 'Point A',
+                                    map: map,
+                                    draggable: true
+                                });
+
+                                // Update current position info.
+                                updateMarkerPosition(latLng);
+                                geocodePosition(latLng);
+
+                                // Add dragging event listeners.
+                                google.maps.event.addListener(marker, 'dragstart', function() {
+                                    updateMarkerAddress('Dragging...');
+                                });
+
+                                google.maps.event.addListener(marker, 'drag', function() {
+                                    updateMarkerStatus('Dragging...');
+                                    updateMarkerPosition(marker.getPosition());
+                                });
+
+                                google.maps.event.addListener(marker, 'dragend', function() {
+                                    updateMarkerStatus('Drag ended');
+                                    geocodePosition(marker.getPosition());
+                                });
+                                }
+
+                                // Onload handler to fire off the app.
+                                google.maps.event.addDomListener(window, 'load', initialize);
+
+                                // $("#input-harga").on('keyup', function() {
+                                //     // alert("oops!");
+                                //     $('#input-harga-label:last').text(format($(this).val()));
+                                // });
+                            </script>
+                            <script
+                            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB35N54GJSZlfnaC1HWjWjoExGy2JuXroc&callback=initMap&libraries=&v=weekly"
+                            async
+                          ></script>
+
+
+                        </div>
+                    </div>
+                      
                 </form>
-            </div>
-        </div>
     </div>
 </div>
 </div>
