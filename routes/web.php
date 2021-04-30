1e4635a9d5118adb4ee1e2c5634ab9b779c72bd6
Kukuh Setya Nugraha <kakadz.pro@gmail.com>
2021-04-30 22:02:23 +0700

    fix bug routes case sensitif on linux

--- routes/web.php
+++ routes/web.php
@@ -29,36 +29,36 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //halaman admin fixed
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function() {
 
-    Route::resource('admin/paket','App\Http\Controllers\AdminpaketController');
+    Route::resource('admin/paket','App\Http\Controllers\AdminPaketController');
     //menu inventaris
     Route::resource('admin/inventaris','App\Http\Controllers\AdminInventarisController');
-    Route::resource('admin/jenisalat','App\Http\Controllers\AdminjenisalatController');
+    Route::resource('admin/jenisalat','App\Http\Controllers\AdminJenisalatController');
     //menu letakserver
     Route::resource('admin/letakserver','App\Http\Controllers\AdminletakserverController');
     //menu pendapatan
-    Route::resource('admin/pendapatan','App\Http\Controllers\AdminpendapatanController');
+    Route::resource('admin/pendapatan','App\Http\Controllers\AdminPendapatanController');
 
     //Route::get('admin/cetak_pdf', 'CetakController@cetak_pdf');
     Route::get('admin/cetak/cetak_paket', 'App\Http\Controllers\CetakController@cetak_paket');
     Route::get('admin/cetak/cetak_letakserver', 'App\Http\Controllers\CetakController@cetak_letakserver');
 
-    Route::resource('admin/jenispendapatan','App\Http\Controllers\AdminjenispendapatanController');
+    Route::resource('admin/jenispendapatan','App\Http\Controllers\AdminJenispendapatanController');
 
     //menu pengeluaran
-    Route::resource('admin/pengeluaran','App\Http\Controllers\AdminpengeluaranController');
-    Route::resource('admin/jenispengeluaran','App\Http\Controllers\AdminjenispengeluaranController');
+    Route::resource('admin/pengeluaran','App\Http\Controllers\AdminPengeluaranController');
+    Route::resource('admin/jenispengeluaran','App\Http\Controllers\AdminJenispengeluaranController');
 
     //menu pengeluaran
-    Route::resource('admin/pelanggan','App\Http\Controllers\AdminpelangganController');
+    Route::resource('admin/pelanggan','App\Http\Controllers\AdminPelangganController');
 
 
     //menu tagihan
-    Route::resource('admin/tagihan','App\Http\Controllers\AdmintagihanController');
-    Route::get('admin/tagihan/{id}/bayar', 'App\Http\Controllers\AdmintagihanController@bayar');
+    Route::resource('admin/tagihan','App\Http\Controllers\AdminTagihanController');
+    Route::get('admin/tagihan/{id}/bayar', 'App\Http\Controllers\AdminTagihanController@bayar');
 
 
     //menu rekap
-    Route::resource('admin/rekap','App\Http\Controllers\AdminrekapController');
+    Route::resource('admin/rekap','App\Http\Controllers\Adminrekapcontroller');
 
 
 
